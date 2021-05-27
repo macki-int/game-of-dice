717dd39c79e64a1b4d1ce6afbf0037a1791212a3
Marek <macki@int.pl>
2021-05-27 08:16:54 +0200

    add round class

---
+++ src/com/mj/model/Round.java
@@ -0,0 +1,26 @@
+package com.mj.model;
+
+import java.util.List;
+import java.util.Map;
+
+public class Round {
+    private int roundNumber;
+    private Map<Player, List<Integer>> diceThrowMap;
+
+
+    public int getRoundNumber() {
+        return roundNumber;
+    }
+
+    public void setRoundNumber(int roundNumber) {
+        this.roundNumber = roundNumber;
+    }
+
+    public Map<Player, List<Integer>> getDiceThrowMap() {
+        return diceThrowMap;
+    }
+
+    public void setDiceThrowMap(Map<Player, List<Integer>> diceThrowMap) {
+        this.diceThrowMap = diceThrowMap;
+    }
+}
