9d77c85b27b40d9a12f9a3061bfb52d47d643cd6
Marek <macki@int.pl>
2021-05-27 08:18:10 +0200

    .

---
+++ src/com/mj/service/Play.java
@@ -0,0 +1,8 @@
+package com.mj.service;
+
+import java.util.List;
+
+public interface Play {
+    List<Integer> playDice();
+
+}
---
+++ src/com/mj/service/PlayImpl.java
@@ -0,0 +1,28 @@
+package com.mj.service;
+
+import com.mj.model.DiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayImpl implements Play {
+
+    @Override
+    public List<Integer> playDice() {
+        List<Integer> numbers = new ArrayList<>();
+
+        int sumThrowDice = 0;
+        for (int i = 1; i <= 10; i++) {
+            sumThrowDice = diceThrow();
+            sumThrowDice += diceThrow();
+            numbers.add(sumThrowDice);
+            sumThrowDice = 0;
+        }
+        return numbers;
+    }
+
+    private int diceThrow() {
+        DiceImpl dice = new DiceImpl(6);
+        return dice.diceRoll();
+    }
+}
