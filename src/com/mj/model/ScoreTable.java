a7b5f427b0e2f442c8b687e83665c79919e56a33
Marek <macki@int.pl>
2021-05-28 00:22:45 +0200

    add in constructor create new List

--- src/com/mj/model/ScoreTable.java
+++ src/com/mj/model/ScoreTable.java
@@ -1,9 +1,14 @@
 package com.mj.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ScoreTable {
     private List<Round> roundList;
+
+    public ScoreTable() {
+        this.roundList = new ArrayList<>();
+    }
 
     public List<Round> getRoundList() {
         return roundList;
