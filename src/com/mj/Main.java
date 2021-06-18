d20d82ad96fc46f62b817b231dd8d7c64d157a29
macki-int <macki@int.pl>
2021-06-18 07:20:02 +0200

    change foreach for generate playDice on simple for

--- src/com/mj/Main.java
+++ src/com/mj/Main.java
@@ -6,10 +6,7 @@ import com.mj.model.ScoreTable;
 import com.mj.service.Play;
 import com.mj.service.PlayImpl;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
 
@@ -28,17 +25,36 @@ public class Main {
 
             Map<Player, List<Integer>> diceThrowOneRoundResultMap = new HashMap<>();
 
-            for (Player player: playerList) {
+            for (int j = 0; j < playerList.size(); j++) {
                 List<Integer> playResultList = playImpl.playDice();
-                diceThrowOneRoundResultMap.put(player, playResultList);
+                diceThrowOneRoundResultMap.put(playerList.get(j), playResultList);
             }
 
             round.setDiceThrowMap(diceThrowOneRoundResultMap);
             scoreTable.addRoundToList(round);
         }
 
-        for (Round r: scoreTable.getRoundList()) {
+
+        for (Round r : scoreTable.getRoundList()) {
+//            int idx = r.getDiceThrowMap().size();
+
             System.out.println(r.getRoundNumber() + " " + r.getDiceThrowMap());
+//            for (int i = 0; i < idx; i++) {
+//                int i1 = i;
+//                r.getDiceThrowMap().keySet()
+//                        .stream()
+//                        .filter(player -> player.equals(playerList.get(i1)))
+//                        .forEach(player -> System.out.println(player.getName()));
+//                System.out.println("----------");
+
+
+//                r.getDiceThrowMap().keySet().forEach(player -> System.out.println(player.getName()));
+//                System.out.println(r.getDiceThrowMap().get(playerList.get(i)));
+
+            //                r.getDiceThrowMap().forEach((player, integers) -> {
+//                    System.out.println(player.getName() + " " + integers);
+//                });
+//            }
         }
     }
 }
